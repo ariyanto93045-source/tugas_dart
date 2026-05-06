b00b6702480b3bce7bdc024185d9f937d2df2001
ariyanto93045-source <ariyanto93045@gmail.com>
2026-05-06 09:20:07 +0700

    tugas1

---
+++ tugas1.dart
@@ -0,0 +1,46 @@
+void main (){
+    
+  // Data anggota
+  String nama = "Ariyanto";
+  int umur = 25;
+  double tinggiBadan = 175.7;
+  bool statusAktif = true;
+
+
+   // List buku favorit
+  List<String> bukuFavorit = [
+    "Masakan",
+    "Hewan" ];
+
+// List Marital 
+   List<String> StatusMArital= [
+    "Nikah",
+    "Lajang"
+   ];
+
+
+    // Map informasi tambahan
+  Map<String, String> infoTambahan = {
+    "alamat": "Jakarta",
+    "profesi": "Wirausaha"
+  };
+   
+  print("=== Biodata Anggota Klub Buku ===");
+
+  print("nama           : $nama");
+  print("umur           : $umur");
+  print("tinggiBadan    : $tinggiBadan cm");
+  print("StatusAktif    : ${statusAktif ? "Aktif" : "Tidak Aktif"}");
+  print("bukuFavorit    : $bukuFavorit");
+  print("bukufavorit    : ${bukuFavorit[0]}");
+  print("bukufavorit    : ${bukuFavorit[1]}");
+  
+  print("=== Informasi Tambahan  ===");
+
+  print("Nama: ${biodata['nama']}");
+}
+
+
+    
+    
+   
